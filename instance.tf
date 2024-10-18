fa5e8d86f3d204839e83b9cbedea295881863af4
Amar Nagargoje <amargithub@gmail.com>
2024-10-18 15:45:10 -0400

    fix

--- instance.tf
+++ instance.tf
@@ -1,21 +1,21 @@
-resource "aws_instance" "webapp_instance" {
-  ami                         = var.ami_id
-  instance_type               = var.instance_type
-  security_groups             = var.security_group_name
-  associate_public_ip_address = true
-  subnet_id                   = aws_subnet.public_subnet[0].id
+# resource "aws_instance" "webapp_instance" {
+#   ami                    = var.ami_id
+#   instance_type          = var.instance_type
+#   subnet_id              = aws_subnet.public_subnet[0].id
+#   vpc_security_group_ids = [var.security_group_id]
 
-  root_block_device {
-    volume_size           = 25
-    volume_type           = "gp2"
-    delete_on_termination = true
-  }
+#   root_block_device {
+#     volume_size           = 25
+#     volume_type           = "gp2"
+#     delete_on_termination = true
+#   }
 
-  disable_api_termination = false
+#   disable_api_termination = false
 
-  tags = {
-    Name = "WebAppInstance"
-  }
-}
+#   tags = {
+#     Name = "WebAppInstance"
+#   }
+#   associate_public_ip_address = true
+# }
 
 
